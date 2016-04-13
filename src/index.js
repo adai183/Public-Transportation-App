961952ba3cf9d186640ad5faf2acbf1df4a10da1
adai183 <andreas.daiminger@gmail.com>
2016-04-13 12:48:06 +0200

    hook up serviceworker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
+/**
+ *
+ * app.js
+ *
+ * This is the entry file for the application, mostly just setup and boilerplate
+ * code. Routes are configured at the end of this file!
+ *
+ */
+
+// Load the ServiceWorker
+import '../serviceworker.js';
+
+// Check for ServiceWorker support before trying to install it
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/serviceworker.js').then(() => {
+    // Registration was successful
+    console.log('ServiceWorker registration was successful');
+  }).catch(() => {
+    // Registration failed
+  });
+} else {
+  // No ServiceWorker Support
+}
+
+
+// Import all the third party stuff
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import promise from 'redux-promise';
 
+// Import components and reducers
 import App from './components/app';
 import reducers from './reducers';
 
